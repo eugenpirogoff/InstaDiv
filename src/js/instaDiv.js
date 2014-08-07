0dbdabd40cd439c9810a2db115b2dc419ac6a7e2
Peter Fessel <peter.fessel@live.de>
2014-08-07 22:11:56 +0200

    - make insta_div container fit exactly in the browser window - delete extra p elements around imgs, that's not needed; also delete some corresponding css - resize images, so that an exact number of images fit into the window; images are still jolted horizontally at certain window widths - images keep their original proportions and are both horizontally and vertically centered - It was necessary to nest the images in an extra outer div again to do this (instead of p element before). - a quick fix for traversing the array of loaded images again from the beginning in case there are more images needed than there are images loaded

--- src/js/instaDiv.js
+++ src/js/instaDiv.js
@@ -1,5 +1,9 @@
 (function( $ ) {
 
+  // 'constants'
+  var BACKGROUNDSQUARE_MAX_WIDTH = 150;
+  var BACKGROUNDSQUARE_MAX_HEIGHT = 149;
+
   $.fn.instaDiv = function(instagram_user_id, access_token) {
     $.ajax({
       type: "GET",
@@ -21,7 +25,7 @@
 
   var ajax_success = function(data) {
     images = store_images(data);
-    append_to_div(images);
+    resize_elements();
   }
 
   function store_images(data) {
@@ -36,27 +40,40 @@
     return images;
   }
 
-  function resize_div() {
+  function resize_elements() {
     $insta_div = $(".insta_div");
 
-    // 150 for thumbnail size, 350 for low resolution, 450 for high resolutiom
-    $insta_div.css('width', window.innerWidth+150);
-    $insta_div.css('height', window.innerHeight+150);
-  }
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
 
-  function append_to_div(images) {
-    $insta_div = $(".insta_div");
+    var horizontalNumberOfSquares = Math.floor(windowWidth / BACKGROUNDSQUARE_MAX_WIDTH) + 1;
+    var verticalNumberOfSquares = Math.floor(windowHeight / BACKGROUNDSQUARE_MAX_HEIGHT) + 1;
 
-    for(var element in images) {
-      if(images.hasOwnProperty(element) && (typeof images[element] != 'function')) {
-        $insta_div.append("<p class='crop_thumb'>" +
-            "<img class='image_thumb' src='" + images[element] + "' /></p>");
+    var backgroundSquareWidth = windowWidth / horizontalNumberOfSquares;
+    var backgroundSquareHeight = Math.floor(windowHeight / verticalNumberOfSquares) + 1;
+
+    if (! $('.insta_div').length ) {
+      $('body').append('<div class="insta_div"></div>');
+    }
+    $insta_div.empty();
+    // set width and height of the square container to match the window dimensions
+    $insta_div.css('width', windowWidth);
+    $insta_div.css('height', windowHeight);
+    // add the appropriate number of squares to fill the background
+    for ( i = 0; i < horizontalNumberOfSquares * verticalNumberOfSquares; i++ ) {
+      var index = i;
+      while (index >= images.length) {
+        index = index - images.length;
+      }
+      if (typeof images[index] != 'function') {
+        var square = $insta_div.append("<div class='crop_thumb'><img class='image_thumb' src='" + images[index] + "' /></div>").children('.crop_thumb').last();
+        $(square).css('width', backgroundSquareWidth);
+        $(square).css('height', backgroundSquareHeight);
       }
     }
-    resize_div();
   }
 
   $(window).resize(function() {
-    resize_div();
+    resize_elements();
   });
 })( jQuery );
